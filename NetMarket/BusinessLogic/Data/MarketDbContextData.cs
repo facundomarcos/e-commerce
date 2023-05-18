4d43b142327375029d20663a2c2ee9503065ae04
facundomarcos <facundomarcos@live.com.ar>
2023-05-18 11:32:03 -0300

    cap 22

---
+++ NetMarket/BusinessLogic/Data/MarketDbContextData.cs
@@ -0,0 +1,71 @@
+﻿
+
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Data
+{
+    public class MarketDbContextData
+    {
+        public static async Task CargarDataAsync(MarketDbContext context, ILoggerFactory loggerFactory)
+        {
+			try
+			{
+				if (!context.Marca.Any())
+				{
+					var marcaData = File.ReadAllText("../BusinessLogic/CargarData/marca.json");
+					var marcas = JsonSerializer.Deserialize<List<Marca>>(marcaData);
+
+					foreach (var marca in marcas)
+					{
+						context.Marca.Add(marca);
+
+					}
+
+					await context.SaveChangesAsync();
+				}
+
+                if (!context.Categoria.Any())
+                {
+                    var categoriaData = File.ReadAllText("../BusinessLogic/CargarData/categoria.json");
+                    var categorias = JsonSerializer.Deserialize<List<Categoria>>(categoriaData);
+
+                    foreach (var categoria in categorias)
+                    {
+                        context.Categoria.Add(categoria);
+
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Producto.Any())
+                {
+                    var productoData = File.ReadAllText("../BusinessLogic/CargarData/producto.json");
+                    var productos = JsonSerializer.Deserialize<List<Producto>>(productoData);
+
+                    foreach (var producto in productos)
+                    {
+                        context.Producto.Add(producto);
+
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
+			catch (Exception e)
+			{
+
+                var logger = loggerFactory.CreateLogger<MarketDbContextData>();
+                logger.LogError(e.Message);
+			}
+        }
+    }
+}
--- NetMarket/WebApi/Program.cs
+++ NetMarket/WebApi/Program.cs
@@ -1,15 +1,42 @@
 
 
+using BusinessLogic.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
 namespace WebApi;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
+        var host = CreateHostBuilder(args).Build();
+        using (var scope = host.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+            try
+            {
+                var context = services.GetRequiredService<MarketDbContext>();
+                await context.Database.MigrateAsync();
+
+                await MarketDbContextData.CargarDataAsync(context, loggerFactory);
+            }
+            catch (Exception e)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(e, "Errores en el proceso de migracion");
+            }
+        }
+
+        host.Run();
+
     }
 
     private static IHostBuilder CreateHostBuilder(string[] args)
