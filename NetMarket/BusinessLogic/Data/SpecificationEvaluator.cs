9a72f0af707fc3749b3f913d12d50e2900326f93
facundomarcos <facundomarcos@live.com.ar>
2024-05-17 12:59:32 -0300

    ordenamiento

--- NetMarket/BusinessLogic/Data/SpecificationEvaluator.cs
+++ NetMarket/BusinessLogic/Data/SpecificationEvaluator.cs
@@ -11,16 +11,27 @@ namespace BusinessLogic.Data
 {
     public class SpecificationEvaluator<T> where T : ClaseBase
     {
-        public static IQueryable<T> GetQuery(IQueryable<T> inputQuery,ISpecification<T> spect)
-        { 
-        if(spect.Criteria != null)
+        public static IQueryable<T> GetQuery(IQueryable<T> inputQuery, ISpecification<T> spect)
+        {
+            if (spect.Criteria != null)
             {
                 inputQuery = inputQuery.Where(spect.Criteria);
+            }
+
+            if (spect.OrderBy != null)
+            {
+                inputQuery = inputQuery.OrderBy(spect.OrderBy);
+
+            }
+            if (spect.OrderByDescending != null)
+            {
+                inputQuery = inputQuery.OrderByDescending(spect.OrderByDescending);
+
             }
             inputQuery = spect.Includes.Aggregate(inputQuery, (current, include) => current.Include(include));
             return inputQuery;
         }
 
-       
+
     }
 }
--- NetMarket/Core/Specifications/BaseSpecification.cs
+++ NetMarket/Core/Specifications/BaseSpecification.cs
@@ -19,11 +19,27 @@ namespace Core.Specifications
         public Expression<Func<T, bool>> Criteria { get; }
 
         public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
-    
+
+
+
         protected void AddInclude(Expression<Func<T, object>> includeExpression) 
         {
             Includes.Add(includeExpression);
         }
-    
+
+        public Expression<Func<T, object>> OrderBy {get; private set;}
+
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+
+        }
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderBy = orderByDescExpression;
+
+        }
     }
 }
