83f16bcbafe4f8c666485bcb990ae4d62111b67e
facundomarcos <facundomarcos@live.com.ar>
2024-06-01 20:56:15 -0300

    seed usuario

---
+++ NetMarket/BusinessLogic/Data/SeguridadDbContextData.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Data
+{
+    public class SeguridadDbContextData
+    {
+        public static async Task SeedUserAsync(UserManager<Usuario> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var usuario = new Usuario
+                {
+                    Nombre = "Facundo",
+                    Apellido = "Marcos",
+                    UserName = "facundomarcos",
+                    Direccion = new Direccion
+                    {
+                        Calle = "Los Patos 2522",
+                        Ciudad = "La Plata",
+                        CodigoPostal = "1900"
+
+                    }
+                };
+                await userManager.CreateAsync(usuario, "Q1w2e3r4$");
+            }
+        }
+    }
+}
