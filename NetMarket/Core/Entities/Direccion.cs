2fbea0b3f280d6ecf279af18e7250580e8763f9e
facundomarcos <facundomarcos@live.com.ar>
2024-06-01 20:31:04 -0300

    clases usuario y direccion

---
+++ NetMarket/Core/Entities/Direccion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Direccion
+    {
+        public int Id { get; set; }
+        public string Calle { get; set; }
+        public string Ciudad { get; set; }  
+public string CodigoPostal { get; set; }
+        public string UsuarioId { get; set; }
+        Usuario Usuario { get; set; }
+    }
+}
